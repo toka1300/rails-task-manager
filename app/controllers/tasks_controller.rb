ad62d0bcb89240fcc091a56ae9aa5a371d4521a0
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-09 13:31:36 -0400

    working still

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,9 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @tasks = Task.all
+    @id = params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get 'index', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
